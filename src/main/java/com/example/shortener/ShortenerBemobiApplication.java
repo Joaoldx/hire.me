a863b2e9cbfcebce38a6bc884964d6c4262e79a0
João Luís <j13domingos@gmail.com>
2018-06-03 15:53:23 -0300

    Initial commit

---
+++ src/main/java/com/example/shortener/ShortenerBemobiApplication.java
@@ -0,0 +1,12 @@
+package com.example.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenerBemobiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenerBemobiApplication.class, args);
+	}
+}
